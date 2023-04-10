b9f499006c48708ce0b6bc9fc653e33afb369ffb
Martin Larsson <marla070@student.liu.se>
2023-04-10 18:50:42 +0200

    lyrist API implemented, added temporary input box for fetching lyrics pending spotify fixing their API dashboard

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,20 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const passport = require('./passport');
+const session = require('express-session');
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(
+  session({
+    secret: '123',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false, maxAge: 24 * 60 * 60 * 1000 }, // 24 hours
+  })
+);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -37,3 +48,20 @@ app.get(
     res.redirect('/');
   }
 );
+
+app.get('/api/lyrics/:artistName/:trackName', async (req, res) => {
+  const { artistName, trackName } = req.params;
+
+  try {
+    //const apiUrl = `https://api.lyrics.ovh/v1/${artistName}/${trackName}`;
+    const apiUrl = `https://lyrist.vercel.app/api/${trackName}/${artistName}`;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    res.json(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'An error occurred while fetching lyrics' });
+  }
+});
+
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,61 @@
-import React from 'react';
-import LoginBox from './LoginBox';
+import React, { useState } from 'react';
 import BackgroundSlideshow from './BackgroundSlideshow';
+import LoginBox from './LoginBox';
 import './App.css';
 
 function App() {
+  const [trackName, setTrackName] = useState('');
+  const [artistName, setArtistName] = useState('');
+  const [lyrics, setLyrics] = useState('');
+
+  async function fetchLyrics(trackName, artistName) {
+    try {
+      const apiUrl = `http://localhost:3001/api/lyrics/${artistName}/${trackName}`;
+      //const response = await fetch(`https://lyrist.vercel.app/api/${trackName}/${artistName}`);
+      const response = await fetch(apiUrl);
+
+      if (!response.ok) {
+        console.error('Error fetching lyrics:', response.status, response.statusText);
+        return;
+      }
+      const data = await response.json();
+      return data.lyrics;
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  
+  
+
+  
+  const handleGetLyrics = async () => {
+    const fetchedLyrics = await fetchLyrics(trackName, artistName);
+    setLyrics(fetchedLyrics);
+  };
+
   return (
     <div className="app">
       <BackgroundSlideshow />
       <div className="login-container">
         <LoginBox />
+        <div className="lyrics-input">
+          <input
+            type="text"
+            placeholder="Track name"
+            value={trackName}
+            onChange={(e) => setTrackName(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Artist name"
+            value={artistName}
+            onChange={(e) => setArtistName(e.target.value)}
+          />
+          <button onClick={handleGetLyrics}>Get Lyrics</button>
+        </div>
+        <div className="lyrics-output">
+          <pre>{lyrics}</pre>
+        </div>
       </div>
     </div>
   );
